b511a36026f7825f4652d886019cb473146b20c7
Gxyloj <48832052+Gxyloj@users.noreply.github.com>
2022-08-29 10:52:46 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,3 +15,11 @@ app
 })
   .use(store)
   .mount('#app')
+
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
