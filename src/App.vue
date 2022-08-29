16da6280be08e128795f42a0d690ac7e0ab7589f
Gxyloj <48832052+Gxyloj@users.noreply.github.com>
2022-08-29 10:47:13 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { defineComponent } from 'vue'
+  import { ElConfigProvider } from 'element-plus'
+
+  import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+
+  export default defineComponent({
+    components: {
+      ElConfigProvider,
+    },
+    setup() {
+      return {
+        locale: zhCn,
+      }
+    },
+  })
 
 </script>
 
