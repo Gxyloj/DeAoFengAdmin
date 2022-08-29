20bfbde066eac3ec2d7ce3d74f5431307ff1ea49
Gxyloj <48832052+Gxyloj@users.noreply.github.com>
2022-08-29 10:50:42 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import './style.css'
 import App from './App.vue'
 import router from "./router/index.js";
 import store from "./store/index.js";
-
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const app = createApp(App)
 
 app
   .use(router)
+  .use(ElementPlus, {
+  locale: zhCn,
+})
   .use(store)
   .mount('#app')
